0f2d452641bd19a4c45838224c331e8569208b6e
Rakshith <rakkshith2001@gmail.com>
2024-07-31 19:06:54 +0530

    finished project

--- src/app/api/validate/route.ts
+++ src/app/api/validate/route.ts
@@ -1,5 +1,4 @@
 import { NextRequest, NextResponse } from 'next/server';
-import fetch from 'node-fetch';
 import { createEmail } from "@/app/lib/actions/email.action";
 
 interface EmailValidationResponse {

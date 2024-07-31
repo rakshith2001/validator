68817f526900e9662d926e05e85dbf895424032d
Rakshith <rakkshith2001@gmail.com>
2024-07-31 19:13:14 +0530

    finished project

--- src/app/api/validate/route.ts
+++ src/app/api/validate/route.ts
@@ -31,13 +31,3 @@ export async function POST(request: NextRequest) {
     return NextResponse.json({ message: 'Email is invalid ❌' }, { status: 400 });
   }
 }
-
-export async function handler(request: NextRequest) {
-  if (request.method === 'POST') {
-    return POST(request);
-  } else {
-    return new NextResponse('Method Not Allowed', { status: 405, headers: { Allow: 'POST' } });
-  }
-}
-
-export { handler as default };
